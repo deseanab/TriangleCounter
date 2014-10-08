4736b2b5589e33fe0bc9ae6e4d019bfe2cfbbd91
pcoonan <patrickcoonan95@gmail.com>
2014-10-08 19:33:47 -0400

    Added more to secondPass

--- 1.0.cpp
+++ 1.0.cpp
@@ -13,6 +13,7 @@ using namespace std;
 
 int countLines(char fileName[]);
 void fillVector(char s[], vector<pair<int, int> > &v);
+void fillVector2(char s[], vector<pair<int, int> > &v);
 void printVector(vector<pair<int, int> > & myVec);
 int firstPass(vector<pair<int, int> > &v1, vector<pair<int, int> > &v2);
 typedef map<int, set<int> >::iterator it_type;
@@ -27,14 +28,14 @@ int main(int argc, char *fname[]){
     
     clock_t startTime = clock();
     
-    fillVector(fname[1], v1);
-    sort(v1.begin(), v1.end());
-    v1.erase(unique(v1.begin(),v1.end()), v1.end());
+    fillVector2(fname[1], v1);
+    //sort(v1.begin(), v1.end());
+    //v1.erase(unique(v1.begin(),v1.end()), v1.end());
     
-    cout << "original vector of pairs:" << endl;
-    printVector(v1);
+    //cout << "original vector of pairs:" << endl;
+    //printVector(v1);
     
-    cout << firstPass(v1,v2) << " triangles were found." << endl;
+    //cout << firstPass(v1,v2) << " triangles were found." << endl;
     secondPass(-1, -1, 3, count);
     cout << count << endl;
 
@@ -92,19 +93,20 @@ void secondPass(int beginning, int next, int num_sides, int &count){
 		for(it_type iterator = myMap.begin(); iterator != myMap.end(); iterator++){
 			int value = iterator->first;
 //			secondPass(value, next, num_sides, count);
-			for(set<int>::iterator it = myMap[value].begin; it != myMap[value].end; it++){
+			set<int>::iterator it;
+			for(it = myMap[value].begin(); it != myMap[value].end(); it++){
 				secondPass(beginning, *it, num_sides - 1, count);
 			}
 		}
 	}
 	else{
 		if(num_sides > 1){
-			for(set<int>::iterator it = myMap[next].begin; it != myMap[next].end; it++){
+			for(set<int>::iterator it = myMap[next].begin(); it != myMap[next].end(); it++){
 				secondPass(beginning, *it, num_sides - 1, count);
 			}
 		}
 		else{
-			for(set<int>::iterator it = myMap[next].begin; it != myMap[next].end; it++){
+			for(set<int>::iterator it = myMap[next].begin(); it != myMap[next].end(); it++){
 				if(*it == beginning){
 					count++;
 				}
@@ -155,6 +157,50 @@ void fillVector(char s[], vector<pair<int, int> > &v){
         }
         inputobject.close();
     }
+}
+
+
+void fillVector2(char s[], vector<pair<int, int> > &v){
+	ifstream inputobject;
+	string line = "";
+	inputobject.open(s);
+	if (inputobject.fail()) {
+		cout << "Failed to open file" << endl;
+		inputobject.clear();
+	}
+	else {
+		cout << "opened: " << s << endl << endl;
+		while (getline(inputobject, line)){
+			if (line.at(0) != '#'){
+				istringstream iss(line);
+				int first = 0, second = 0;
+				iss >> first;
+				iss >> second;
+				map <int, set<int> >::iterator it;
+				
+
+				it = myMap.find(first);
+
+				if (it != myMap.end()){
+					myMap[first].insert(second);
+				}
+				else{
+
+					myMap[first].insert(second);
+				}
+
+				pair<int, int> p;
+				p = make_pair(first, second);
+				//                if (first <= second){
+				//                    p = make_pair(first,second);
+				//                } else {
+				//                    p = make_pair(second,first);
+				//                }
+				v.push_back(p);
+			}
+		}
+		inputobject.close();
+	}
 }
 
 int countLines(char fileName[]){
